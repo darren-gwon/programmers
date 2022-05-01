d18c3d66638c736a489e3d613be1f5df8ce0ab64
Darren Gwon <darren.gwon@gmail.com>
2022-05-01 19:34:03 +0900

    프로그래머스 코딩테스트 공부

--- Level1/001-12969.java
+++ Level1/001-12969.java
@@ -1,3 +1,6 @@
+//직사각형 별찍기
+//https://programmers.co.kr/learn/courses/30/lessons/12969
+
 import java.util.Scanner;
 
 class Solution {
--- Level1/002-12954.java
+++ Level1/002-12954.java
@@ -1,3 +1,6 @@
+//x만큼 간격이 있는 n개의 숫자
+//https://programmers.co.kr/learn/courses/30/lessons/12954
+
 class Solution {
     public long[] solution(int x, int n) {
         long[] answer = new long[n];
