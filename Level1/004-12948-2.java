0a503916a938f5d2418b3647e9693fe79ec4f350
Darren Gwon <darren.gwon@gmail.com>
2022-05-01 20:48:53 +0900

    프로그래머스 코딩테스트 공부

---
+++ Level1/004-12948-2.java
@@ -0,0 +1,16 @@
+//핸드폰 번호 가리기
+//https://programmers.co.kr/learn/courses/30/lessons/12948
+
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String phone_number) {      
+        char[] numberCharArr = phone_number.toCharArray();
+
+        for(int i = 0; i<phone_number.length()-4; i++){
+            numberCharArr[i]='*';
+        }
+        
+        return String.valueOf(numberCharArr);
+    }
+}
